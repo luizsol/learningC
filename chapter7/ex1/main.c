3a6cc2afdd25a32befcee174664796c511dd6839
Luiz Sol <luizedusol@gmail.com>
2016-05-22 00:41:11 -0300

    Finished Chapter 6. Stopped at page 315

---
+++ chapter7/ex1/main.c
@@ -0,0 +1,39 @@
+/*
+ * Searches for a person who likes sports but doesn't likes bieber
+ * Source: Head First C Book
+ * Chapter 7, page 313
+ * Author: Luiz Sol
+ * Date: 2016/05/22
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int NUM_ADS = 7;
+char *ADS[] = {
+	"William: SBM GSOH likes sports, TV, dining",
+	"Matt: SWM NS likes art, movies, theater",
+	"Luis: SLM ND likes books, theater, art",
+	"Mike: DWM DS likes trucks, sports and bieber",
+	"Peter: SAM likes chess, working out and art",
+	"Josh: SJM likes sports, movies and theater",
+	"Jed: DBM likes theater, books and dining"
+};
+
+void find(){
+	int i;
+	puts("Search results:");
+	puts("-----------------------------------");
+	
+	for(i = 0; i < NUM_ADS; i++){
+		if (strstr(ADS[i], "sports") && ! strstr(ADS[i], "bieber"))
+			printf("%s\n", ADS[i]);
+	}
+	puts("-----------------------------------");	
+}
+
+int main(){
+	find();
+	return 0;
+}
