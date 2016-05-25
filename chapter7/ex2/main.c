972002a2bec6839a84a8b3dafc1010f668ef5e16
Luiz Sol <luizedusol@gmail.com>
2016-05-24 22:02:33 -0300

    Forget to commit this exercise yesterday. Stopped at page 327

---
+++ chapter7/ex2/main.c
@@ -0,0 +1,58 @@
+/*
+ * Searches for some specific types of men
+ * Source: Head First C Book
+ * Chapter 7, page 321
+ * Author: Luiz Sol
+ * Date: 2016/05/22
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int NUM_ADS = 7;
+char *ADS[] = {
+	"William: SBM GSOH likes sports, TV, dining",
+	"Matt: SWM NS likes art, movies, theater",
+	"Luis: SLM ND likes books, theater, art",
+	"Mike: DWM DS likes trucks, sports and bieber",
+	"Peter: SAM likes chess, working out and art",
+	"Josh: SJM likes sports, movies and theater",
+	"Jed: DBM likes theater, books and dining"
+};
+
+int sports_no_bieber(char *s){
+	return strstr(s, "sports") && !strstr(s, "bieber");
+}
+
+int sports_or_workout(char *s){
+	return strstr(s, "sports") || strstr(s, "workout");
+}
+
+int ns_theater(char *s){
+	return strstr(s, "NS") && strstr(s, "theater");
+}
+
+int arts_theater_or_dining(char *s){
+	return strstr(s, "arts") || strstr(s, "theater") || strstr(s, "dining");
+}
+
+void find(int (*match)(char *s)){
+	int i;
+	puts("Search results:");
+	puts("------------------------------------");
+	for (i = 0; i < NUM_ADS; i++) {
+		if (match(ADS[i])) {
+			printf("%s\n", ADS[i]);
+		}
+	}
+	puts("------------------------------------");
+}
+
+int main(){
+	find(sports_no_bieber);
+	find(sports_or_workout);
+	find(ns_theater);
+	find(arts_theater_or_dining);
+	return 0;
+}
